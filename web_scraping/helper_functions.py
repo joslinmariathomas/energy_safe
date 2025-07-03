04242adbfbe9ad880aae751365e0e4861ce7f832
joslinmariathomas <joslinmariathomas94@gmail.com>
2025-07-04 03:58:35 +1000

    Add helper functions

---
+++ web_scraping/helper_functions.py
@@ -0,0 +1,35 @@
+from bs4 import BeautifulSoup, Tag
+
+
+def find_by_id(soup: BeautifulSoup, tag: str, value: str | None):
+    return soup.find(name=tag, id=value)
+
+
+def find_by_class(soup: BeautifulSoup, tag: str, value: str | None):
+    return soup.find(name=tag, class_=value)
+
+
+def find_by_itemprop(soup: BeautifulSoup, tag: str, value: str | None):
+    return soup.find(name=tag, itemprop=value)
+
+
+def find_tag_only(soup: BeautifulSoup, tag: str, value: str | None = None):
+    return soup.find(name=tag)
+
+
+def cleanup_html_tag(item: str, html_retrieved_value: Tag) -> str:
+    if not html_retrieved_value:
+        return ""
+    if item == "price" and html_retrieved_value:
+        html_retrieved_value = html_retrieved_value.find("div")
+    if item == "posting_date":
+        retrieved_value = (
+            html_retrieved_value.get_text(" ", strip=True)
+            .replace("Posted:", "")
+            .strip()
+        )
+        return retrieved_value
+    html_retrieved_value = (
+        html_retrieved_value.get_text(strip=True) if html_retrieved_value else ""
+    )
+    return html_retrieved_value
